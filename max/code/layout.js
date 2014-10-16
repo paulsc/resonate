f512e1601dfc23d5cd797764de3c81b93fd540ad
paulsc <paul167@gmail.com>
2014-10-16 20:20:40 +0200

    removed crossfade default and added spacer

--- max/code/layout.js
+++ max/code/layout.js
@@ -2,8 +2,9 @@ inlets = 1;
 outlets = 1;
 
 function moveto(index, x, y, z) {
+	var spacer = 1.1;
 	outlet(0, [0, "target", index]);
-	outlet(0, [1, "moveto", x, y, z, 1]);
+	outlet(0, [1, "moveto", x * spacer, y * spacer, z * spacer, 1]);
 	
 }
 
@@ -24,6 +25,12 @@ function msg_int(n)
 			moveto(0, -2, 0, 0);
 			moveto(1, 0, 0, 0);
 			moveto(2, 2, 0, 0);
+			break;
+		case 4:
+			moveto(0, -1, 1, 0);
+			moveto(1, 1, 1, 0);
+			moveto(2, -1, -1, 0);
+			moveto(3, 1, -1, 0);
 			break;
 	}
 }	
